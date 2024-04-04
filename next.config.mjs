1a693d1f0b20ecb154afc69b9061738e821cca63
Piero Toscano <iepvptr@gmail.com>
2024-04-04 02:06:22 -0500

    Refactoring before testing paginator

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,6 @@ const nextConfig = {
     async rewrites() {
         return [
             {
-                source: "/api/:path*",
-                destination: "http://localhost:4000/api/:path*"
-            },
-            {
                 source: "/backend/:path*",
                 destination: "http://localhost:9081/api/:path*"
             }
