1f299b82090e5437706e8e011a8adf5a3ecafbb3
Piero Toscano <iepvptr@gmail.com>
2024-04-06 12:54:05 -0500

    Addition of Main navbar navigator menu

---
+++ src/components/MainMenuUser.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+export default ({ children }: { children: React.ReactNode }) => {
+    return (
+        <div className="content">
+            <div className="navbar">
+                <div className="brand"></div>
+                <div className="communication">
+                    <span className="">
+                        <i className="fa-solid fa-bell"></i>
+                    </span>
+                    <span>
+                        <i className="fa-solid fa-comment"></i>
+                    </span>
+                </div>
+            </div>
+            <div className="sidebar">
+                <div className="sidebar-item">
+                    <span><i className="fa-solid fa-thumbs-up"></i> Mis acuerdos</span>
+                </div>
+                <div className="sidebar-item">
+                    <span><i className="fa-solid fa-file-contract"></i> Mis contratos</span></div>
+                <div className="sidebar-item">
+                    <span><i className="fa-solid fa-gear"></i> Servicios</span>
+                </div>
+                <div className="sidebar-item">
+                    <span><i className="fa-solid fa-envelope"></i> Mis solicitudes</span></div>
+            </div>
+            <div className="">{children}</div>
+        </div>
+    )
+}
