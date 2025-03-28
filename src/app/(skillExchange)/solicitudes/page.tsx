c0c5b00c69edddf5e11645e56c80b13484a44f4e
Piero <iepvptr@gmail.com>
2025-03-28 08:01:33 -0500

    solicitudes: page load

--- src/app/(skillExchange)/solicitudes/page.tsx
+++ src/app/(skillExchange)/solicitudes/page.tsx
@@ -1,6 +1,9 @@
 "use client";
 
-import { actualizarMatchEstado, obtenerDetailsMatchsPrestamistaAndOptionalEstado } from "@/actions/match.actions";
+import {
+  actualizarMatchEstado,
+  obtenerDetailsMatchsPrestamistaAndOptionalEstado,
+} from "@/actions/match.actions";
 import { obtenerServiciosByPrestamista } from "@/actions/servicio.actions";
 import MatchServicioDetailsResponse from "@/interfaces/responsebody/matching/MatchServicioDetailsResponse";
 import ServicioResponse from "@/interfaces/responsebody/servicio/ServicioResponse";
@@ -47,45 +50,54 @@ function loadInformation() {
     }
 } */
 
-const getMatchServicioDetails = () => {
-    return obtenerUsuarioLogged()
-        .then(userLogged => {
-            return obtenerDetailsMatchsPrestamistaAndOptionalEstado(userLogged.id, "solicitado");
-        });
-}
-
-const matchsResource = wrapPromise(getMatchServicioDetails());
-
 export default () => {
-    //const [matchsServicioSelected, setMatchsServicioSelected] = useState<MatchServicioDetailsResponse[]>([]);
-    //const [matchsServicioDetails, setMatchsServicioDetails] = useState<MatchServicioDetailsResponse[]>();
-    //const [servicios, setServicios] = useState<ServicioResponse[]>([]);
-    const [servicio, setServicio] = useState<ServicioResponse>();
-    const [clienteModalData, setClienteModalData] = useState<UsuarioResponse>();
-    const [openModalCliente, setOpenModalCliente] = useState<boolean>(false);
-    const [openModalSolicitudAprobada, setOpenModalSolicitudAprobada] = useState<boolean>(false);
-    const [clienteAceptado, setClienteAceptado] = useState<UsuarioResponse>();
-    const [matchForRechazo, setMatchForRechazo] = useState<MatchServicioDetailsResponse>();
-    const [openModalSolicitudRechazada, setOpenModalSolicitudRechazada] = useState<boolean>(false);
-    const [openModalConfirmSolicitudRechazo, setOpenModalConfirmSolicitudRechazo] = useState<boolean>(false);
-    let matchsServicioDetails = matchsResource.read();
-    let matchsServicioSelected: MatchServicioDetailsResponse[] = (matchsServicioDetails as MatchServicioDetailsResponse[])
-        .filter(match => match.servicio.id === servicio?.id);
-    let servicios: ServicioResponse[];
-    let status = "pending";
-    /* const matchsServicioDetails = matchsData(); */
-    /* if (matchsServicioDetails) {
+  //const [matchsServicioSelected, setMatchsServicioSelected] = useState<MatchServicioDetailsResponse[]>([]);
+  //const [matchsServicioDetails, setMatchsServicioDetails] = useState<MatchServicioDetailsResponse[]>();
+  //const [servicios, setServicios] = useState<ServicioResponse[]>([]);
+  const getMatchServicioDetails = () => {
+    return obtenerUsuarioLogged().then((userLogged) => {
+      return obtenerDetailsMatchsPrestamistaAndOptionalEstado(
+        userLogged.id,
+        "solicitado"
+      );
+    });
+  };
+
+  const [servicio, setServicio] = useState<ServicioResponse>();
+  const [clienteModalData, setClienteModalData] = useState<UsuarioResponse>();
+  const [openModalCliente, setOpenModalCliente] = useState<boolean>(false);
+  const [openModalSolicitudAprobada, setOpenModalSolicitudAprobada] =
+    useState<boolean>(false);
+  const [clienteAceptado, setClienteAceptado] = useState<UsuarioResponse>();
+  const [matchForRechazo, setMatchForRechazo] =
+    useState<MatchServicioDetailsResponse>();
+  const [openModalSolicitudRechazada, setOpenModalSolicitudRechazada] =
+    useState<boolean>(false);
+  const [
+    openModalConfirmSolicitudRechazo,
+    setOpenModalConfirmSolicitudRechazo,
+  ] = useState<boolean>(false);
+  const matchsResource = wrapPromise(getMatchServicioDetails());
+  let matchsServicioDetails = matchsResource.read();
+  let matchsServicioSelected: MatchServicioDetailsResponse[] = (
+    matchsServicioDetails as MatchServicioDetailsResponse[]
+  ).filter((match) => match.servicio.id === servicio?.id);
+  let servicios: ServicioResponse[];
+  let status = "pending";
+
+  /* const matchsServicioDetails = matchsData(); */
+  /* if (matchsServicioDetails) {
         setServicios(matchsServicioDetails.map(m => m.servicio)
             .filter((servicioItem, index, arrayServicios) => index === arrayServicios.findIndex(s => s.id === servicioItem.id)));
     } */
 
-    //let matchsServicioDetails: MatchServicioDetailsResponse[] = [];
+  //let matchsServicioDetails: MatchServicioDetailsResponse[] = [];
 
-    /* useEffect(() => {
+  /* useEffect(() => {
         loadInformation();
     }, []); */
 
-    /* const loadInformation = async () => {
+  /* const loadInformation = async () => {
         await new Promise(resolver => setTimeout(resolver, 6000));
         const userLogged = await obtenerUsuarioLogged();
         try {
@@ -101,111 +113,128 @@ export default () => {
         }
     } */
 
-    const openModalViewProfile = (cliente: UsuarioResponse) => {
-        setClienteModalData(cliente);
-        setOpenModalCliente(true);
+  const openModalViewProfile = (cliente: UsuarioResponse) => {
+    setClienteModalData(cliente);
+    setOpenModalCliente(true);
+  };
+
+  const handleCloseModal = () => {
+    setOpenModalCliente(false);
+  };
+
+  const handleSolicitudAprobada = (cliente: UsuarioResponse) => {
+    setOpenModalSolicitudAprobada(true);
+    setClienteAceptado(cliente);
+  };
+
+  const closeSolicitudAprobada = () => {
+    setOpenModalSolicitudAprobada(false);
+    setClienteAceptado(undefined);
+  };
+
+  const handleConfirmSolicitudForRechazo = (
+    match: MatchServicioDetailsResponse
+  ) => {
+    // Abrir modal de confirmación de rechazar solicitud
+    setOpenModalConfirmSolicitudRechazo(true);
+    setMatchForRechazo(match);
+  };
+
+  const cancelarRechazoSolicitud = () => {
+    setOpenModalConfirmSolicitudRechazo(false);
+    setMatchForRechazo(undefined);
+  };
+
+  const rechazarSolicitud = async () => {
+    // Rechazar solicitud
+    const matchRechazado = await actualizarMatchEstado(matchForRechazo!.id, {
+      estado: "rechazado",
+    });
+    if (matchRechazado) {
+      setOpenModalConfirmSolicitudRechazo(false); // Cerrar modal de confirmar rechazo
+      setMatchForRechazo(undefined);
+
+      // Abrir modal de solicitud rechazada correctamente
+      setOpenModalSolicitudRechazada(true);
     }
+  };
 
-    const handleCloseModal = () => {
-        setOpenModalCliente(false);
-    }
+  const closeSolicitudRechazada = () => {
+    setOpenModalSolicitudRechazada(false);
+  };
 
-    const handleSolicitudAprobada = (cliente: UsuarioResponse) => {
-        setOpenModalSolicitudAprobada(true);
-        setClienteAceptado(cliente);
-    }
+  //const matchsServicioSelected = matchsServicioDetails?.filter(match => match.servicio.id === servicio?.id);
 
-    const closeSolicitudAprobada = () => {
-        setOpenModalSolicitudAprobada(false);
-        setClienteAceptado(undefined);
-    }
+  return (
+    <>
+      <div className="principal">
+        <div className="form-row">
+          <div className="form-control">
+            <label htmlFor="servicio">Servicio:</label>
+            <select
+              name="servicio"
+              value={servicio?.id}
+              onChange={(e) =>
+                setServicio(servicios.find((s) => s.id === e.target.value))
+              }
+            >
+              <option>--Seleccione--</option>
+              {servicios!.map((s) => (
+                <option key={s.id} value={s.id}>
+                  {s.titulo}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
+        <div>
+          {matchsServicioSelected!.length > 0 ? (
+            matchsServicioSelected!.map((m) => (
+              <SolicitudItem
+                key={m.id}
+                match={m}
+                sendModalState={openModalViewProfile}
+                sendSolicitudAprobada={handleSolicitudAprobada}
+                sendSolicitudForRechazo={handleConfirmSolicitudForRechazo}
+              />
+            ))
+          ) : (
+            <>
+              <p>No hay solicitudes por ahora</p>
+              <a className="link-button btn-primary" href="/contratos">
+                Ver mis contratos
+              </a>
+            </>
+          )}
+        </div>
+      </div>
 
-    const handleConfirmSolicitudForRechazo = (match: MatchServicioDetailsResponse) => {
-        // Abrir modal de confirmación de rechazar solicitud
-        setOpenModalConfirmSolicitudRechazo(true);
-        setMatchForRechazo(match);
-    }
-
-    const cancelarRechazoSolicitud = () => {
-        setOpenModalConfirmSolicitudRechazo(false);
-        setMatchForRechazo(undefined);
-    }
-
-    const rechazarSolicitud = async () => {
-        // Rechazar solicitud
-        const matchRechazado = await actualizarMatchEstado(matchForRechazo!.id, {
-            estado: "rechazado"
-        });
-        if (matchRechazado) {
-            setOpenModalConfirmSolicitudRechazo(false); // Cerrar modal de confirmar rechazo
-            setMatchForRechazo(undefined);
-
-            // Abrir modal de solicitud rechazada correctamente
-            setOpenModalSolicitudRechazada(true);
-        }
-    }
-
-    const closeSolicitudRechazada = () => {
-        setOpenModalSolicitudRechazada(false);
-    }
-
-    //const matchsServicioSelected = matchsServicioDetails?.filter(match => match.servicio.id === servicio?.id);
-
-    return (
-        <>
-            <div className="principal">
-                <div className="form-row">
-                    <div className="form-control">
-                        <label htmlFor="servicio">Servicio:</label>
-                        <select name="servicio" value={servicio?.id}
-                            onChange={(e) => setServicio(servicios.find(s => s.id === e.target.value))}>
-                            <option>--Seleccione--</option>
-                            {servicios!.map(s =>
-                                <option key={s.id} value={s.id}>{s.titulo}</option>
-                            )}
-                        </select>
-                    </div>
-                </div>
-                <div>
-                    {matchsServicioSelected!.length > 0 ? matchsServicioSelected!.map(m =>
-                        <SolicitudItem key={m.id}
-                            match={m}
-                            sendModalState={openModalViewProfile}
-                            sendSolicitudAprobada={handleSolicitudAprobada}
-                            sendSolicitudForRechazo={handleConfirmSolicitudForRechazo} />
-                    ) :
-                        <>
-                            <p>No hay solicitudes por ahora</p>
-                            <a className="link-button btn-primary" href="/contratos">Ver mis contratos</a>
-                        </>
-                    }
-                </div>
-            </div>
-
-            {(openModalCliente && !!clienteModalData) &&
-                <ModalVerPerfilUsuario cliente={clienteModalData!} onClose={handleCloseModal} />
-            }
-            {(openModalSolicitudAprobada && !!clienteAceptado) &&
-                <ModalAlert
-                    onClose={closeSolicitudAprobada}>
-                    <p>!Excelente! Tienes un trato con {clienteAceptado?.nombres}</p>
-                    <p>Contáctate para que pueda realizar el pago.</p>
-                </ModalAlert>
-            }
-            {(openModalConfirmSolicitudRechazo && !!matchForRechazo) &&
-                <ModalConfirm
-                    onConfirm={rechazarSolicitud}
-                    onCancel={cancelarRechazoSolicitud}
-                    onClose={cancelarRechazoSolicitud}>
-                    <p>{`¿Está seguro de rechazar la solicitud de ${matchForRechazo?.cliente.nombres}?`}</p>
-                </ModalConfirm>
-            }
-            {openModalSolicitudRechazada &&
-                <ModalAlert
-                    onClose={closeSolicitudRechazada}>
-                    <p>Solicitud rechazada</p>
-                </ModalAlert>
-            }
-        </>
-    )
-}
+      {openModalCliente && !!clienteModalData && (
+        <ModalVerPerfilUsuario
+          cliente={clienteModalData!}
+          onClose={handleCloseModal}
+        />
+      )}
+      {openModalSolicitudAprobada && !!clienteAceptado && (
+        <ModalAlert onClose={closeSolicitudAprobada}>
+          <p>!Excelente! Tienes un trato con {clienteAceptado?.nombres}</p>
+          <p>Contáctate para que pueda realizar el pago.</p>
+        </ModalAlert>
+      )}
+      {openModalConfirmSolicitudRechazo && !!matchForRechazo && (
+        <ModalConfirm
+          onConfirm={rechazarSolicitud}
+          onCancel={cancelarRechazoSolicitud}
+          onClose={cancelarRechazoSolicitud}
+        >
+          <p>{`¿Está seguro de rechazar la solicitud de ${matchForRechazo?.cliente.nombres}?`}</p>
+        </ModalConfirm>
+      )}
+      {openModalSolicitudRechazada && (
+        <ModalAlert onClose={closeSolicitudRechazada}>
+          <p>Solicitud rechazada</p>
+        </ModalAlert>
+      )}
+    </>
+  );
+};
